5efbc16dfff256d66b2c447b44b6cfd8d7b0bc22
Elliot <waqasaps@hotmail.com>
2019-10-12 20:07:17 +0500

    UI Changes

--- js/algorithms.js
+++ js/algorithms.js
@@ -1,1 +1,20 @@
-alert(columns)
+$(document).ready(function() {
+  var width = $(document).width()
+  var height = $(document).height()
+  var p16 = 25
+
+  var columns = Math.floor(width / p16)
+
+  var rows = Math.floor(height / p16) - 2
+  var startingpoint = Math.floor(rows / 2) + '_' + Math.floor(columns * 0.1)
+  var endpoint = Math.floor(rows / 2) + '_' + Math.floor(columns * 0.9)
+
+
+
+function (){
+    
+} 
+
+
+
+})
